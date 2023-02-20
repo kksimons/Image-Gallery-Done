50dafa98f9541adb05d48ec9306b98cbe7e78fe5
Kyle Simons <kksimons@gmail.com>
2023-02-20 16:09:45 -0700

    save images, update array

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,27 +8,32 @@ const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 // final setword clears and returns the default value after pressing the button
 const App = () => {
   const [word, setWord] = useState('');
+  const [images, setImages] = useState([]);
+
+  console.log(images); //can't be in the function because it'll update asynchronously and be a term behind
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
     console.log(word);
-    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
+    fetch(
+      `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`
+    )
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
+        setImages([data, ...images]); //creates brand new array of images, pass new array to setImages function
       })
       .catch((err) => {
         console.log(err);
-      })
+      });
     setWord('');
-  }
- 
+  };
+
   return (
     <div>
-      <Header title="Image Gallery"/>
-      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit}/>
+      <Header title="Image Gallery" />
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
     </div>
   );
-}
+};
 
 export default App;
