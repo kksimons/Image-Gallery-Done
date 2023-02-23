693e7c99e3bde87e503808c5f6912afe1d32fceb
Kyle Simons <kksimons@gmail.com>
2023-02-22 22:02:19 -0700

    enabled pylint and black

--- api/main.py
+++ api/main.py
@@ -2,19 +2,26 @@ import os
 import requests
 from flask import Flask, request
 from dotenv import load_dotenv
+from flask_cors import CORS
 
-load_dotenv(dotenv_path="./.env.local") #to pull the key locally
+load_dotenv(dotenv_path="./.env.local")  # to pull the key locally
 
-UNSPLASH_URL="https://api.unsplash.com/photos/random/"
-UNSPLASH_KEY=os.environ.get("UNSPLASH_KEY", "") #second argument is default
-DEBUG=bool(os.environ.get("DEBUG", True)) #add DEBUG="" to env.local to turn off debug mode as being on be default
+UNSPLASH_URL = "https://api.unsplash.com/photos/random/"
+UNSPLASH_KEY = os.environ.get("UNSPLASH_KEY", "")  # second argument is default
+DEBUG = bool(
+    os.environ.get("DEBUG", True)
+)  # add DEBUG="" to env.local to turn off debug mode as being on be default
 
 if not UNSPLASH_KEY:
-    raise EnvironmentError("UNSPLASH_KEY is missing, please create one and put it in .env.local")
+    raise EnvironmentError(
+        "UNSPLASH_KEY is missing, please create one and put it in .env.local"
+    )
 
 app = Flask(__name__)
+CORS(app)  # to avoid any cross origin errors
 
 app.config["DEBUG"] = DEBUG
+
 
 # if view function in Flask returns a dictionary content-type is autoset to json and returns json object
 @app.route("/new-image")
@@ -22,15 +29,20 @@ def new_image():
     word = request.args.get("query")
 
     headers = {
-        "Accept-Version": "v1", #as per the unsplash docs
-        "Authorization": "Client-ID " + UNSPLASH_KEY
+        "Accept-Version": "v1",  # as per the unsplash docs
+        "Authorization": "Client-ID " + UNSPLASH_KEY,
     }
-    params = {"query": word } #takes the query made from the search box(client) and insert it into the request to the remote unsplash api server
+    params = {
+        "query": word
+    }  # takes the query made from the search box(client) and insert it into the request to the remote unsplash api server
 
-    response = requests.get(url=UNSPLASH_URL, headers=headers, params=params) #result of the get
-    
-    data = (response.json())
-    return data 
+    response = requests.get(
+        url=UNSPLASH_URL, headers=headers, params=params
+    )  # result of the get
+
+    data = response.json()
+    return data
+
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5050) 
+    app.run(host="0.0.0.0", port=5050)
